37c19c4aa146bd802aa672a23de087c34166fba6
withor.dev <withorcello@gmail.com>
2022-08-27 18:09:49 -0400

    feat: inicio calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,123 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+typedef OperadorFunc = double Function(double valor, double operando);
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  MyAppState createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  double result = 0.0;
+  final numberOne = TextEditingController();
+  final numberTwo = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+          body: Material(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                Text(
+                  result.toString(),
+                  style: const TextStyle(
+                      color: Colors.black54,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30),
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                        controller: numberOne,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Primeiro Número'))),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: TextField(
+                        controller: numberTwo,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Segundo Número'))),
+                Padding(
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ElevatedButton(
+                              child: const Text('+', textScaleFactor: 3),
+                              onPressed: () {
+                                setState(() {
+                                  result = double.parse(numberOne.text.isEmpty
+                                          ? numberOne.text
+                                          : '0') +
+                                      double.parse(numberTwo.text.isEmpty
+                                          ? numberTwo.text
+                                          : '0');
+                                });
+                              }),
+                          ElevatedButton(
+                              style: const ButtonStyle(),
+                              child: const Text('-', textScaleFactor: 3),
+                              onPressed: () {
+                                setState(() {
+                                  result = double.parse(numberOne.text.isEmpty
+                                          ? numberOne.text
+                                          : '0.0') -
+                                      double.parse(numberTwo.text.isEmpty
+                                          ? numberTwo.text
+                                          : '0.0');
+                                });
+                              }),
+                          ElevatedButton(
+                              child: const Text('x', textScaleFactor: 3),
+                              onPressed: () {
+                                setState(() {
+                                  result = double.parse(numberOne.text.isEmpty
+                                          ? numberOne.text
+                                          : '0') *
+                                      double.parse(numberTwo.text.isEmpty
+                                          ? numberTwo.text
+                                          : '0');
+                                });
+                              }),
+                          ElevatedButton(
+                              child: const Text('/', textScaleFactor: 3),
+                              onPressed: () {
+                                setState(() {
+                                  result = double.parse(
+                                          !numberOne.text.isNotEmpty
+                                              ? numberOne.text
+                                              : '0') /
+                                      double.parse(!numberTwo.text.isNotEmpty
+                                          ? numberTwo.text
+                                          : '0');
+                                });
+                              }),
+                        ]))
+              ])),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                result = 0.0;
+                numberOne.text = '';
+                numberTwo.text = '';
+              });
+            },
+            child: const Text('C', textScaleFactor: 2),
+          )),
     );
   }
 }
